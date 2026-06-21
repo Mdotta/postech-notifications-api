34354a9fdaa97acfff6d7e6659be14ca08f88f7b
Mdotta <md.correa@outlook.com>
2026-06-21 15:14:14 -0300

    cleanup: remove old api project

--- src/Postech.Notifications.Api/Infrastructure/DynamoDB/DynamoDbSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace Postech.Notifications.Api.Infrastructure.DynamoDB;
-
-public class DynamoDbSettings
-{
-    public bool UseDynamoDB { get; set; }
-    public string TableName { get; set; } = "postech_notifications_event_logs";
-}
--- src/Postech.Notifications.Api/Infrastructure/MongoDB/Repositories/IEventLogRepository.cs
+++
@@ -1,8 +0,0 @@
-using Postech.Notifications.Api.Infrastructure.MongoDB.Documents;
-
-namespace Postech.Notifications.Api.Infrastructure.MongoDB.Repositories;
-
-public interface IEventLogRepository
-{
-    Task InsertAsync(EventLogDocument document, CancellationToken cancellationToken = default);
-}
